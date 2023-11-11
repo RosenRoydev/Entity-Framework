23f7c0e5f49d17ed6f5b47604176802196b378ab
RosenRoydev <122979363+RosenRoydev@users.noreply.github.com>
2023-11-11 22:37:12 +0200

    new examples

--- 02. Age Restriction_Skeleton/BookShop/StartUp.cs
+++ 02. Age Restriction_Skeleton/BookShop/StartUp.cs
@@ -5,6 +5,7 @@
     using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
     using System.Linq;
     using BookShop.Models.Enums;
+    using System.Globalization;
 
     public class StartUp
     {
@@ -12,8 +13,8 @@
         {
             using var db = new BookShopContext();
             //DbInitializer.ResetDatabase(db);
-           
-            string result = GetBooksByPrice(db);
+           string input =Console.ReadLine();
+            string result = GetAuthorNamesEndingIn(db, input);
             Console.WriteLine(result);
         }
 
@@ -55,6 +56,55 @@
                 b.Price,
             }).OrderByDescending(b => b.Price).ToList();
             string result = string.Join(Environment.NewLine, books.Select(b => $"{b.Title} - ${b.Price:f2}"));
+            return result;
+        }
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+
+            var books = context.Books.Where(b => b.ReleaseDate.Value.Year != year).
+                OrderBy(b => b.BookId).
+                Select(b => b.Title).ToList();
+
+            string result = string.Join(Environment.NewLine, books);
+            return result;
+        }
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[]genres = input.Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(g =>g.ToLower()).ToArray();
+            var books = context.Books
+                .Where(b => b.BookCategories.Any(bc => genres.Contains(bc.Category.Name.ToLower())))
+                .OrderBy(b => b.Title)
+                .ToList();
+            string result = string.Join(Environment.NewLine,books.Select(b=> $"{b.Title}"));
+            return result;
+        }
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            var parsedDate = DateTime.ParseExact(date, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            var books = context.Books
+               .Where(b => b.ReleaseDate < parsedDate)
+               .Select(b => new
+               {
+                   b.Title,
+                   b.EditionType,
+                   b.Price,
+                   b.ReleaseDate
+               })
+               .OrderByDescending(b => b.ReleaseDate);
+
+            string result = string.Join(Environment.NewLine, books.Select(b => $"{b.Title} - {b.EditionType} - " +
+            $"${b.Price}"));
+            return result;
+        }
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            var authors = context.Authors.
+                Where(a => a.FirstName.EndsWith(input)).
+                AsEnumerable().
+                Select(a => new {FullName = $"{a.FirstName} {a.LastName}"}).
+                OrderBy(a => a.FullName).ToList();
+
+            string result = string.Join(Environment.NewLine,authors.Select(a => $"{a.FullName}"));
             return result;
         }
     }
