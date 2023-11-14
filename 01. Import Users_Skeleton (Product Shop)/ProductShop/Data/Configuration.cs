cdd357fd138f110b584ed6150f281c0245b1825f
RosenRoydev <122979363+RosenRoydev@users.noreply.github.com>
2023-11-14 17:21:17 +0200

    Product Shop
    Export and import json files

---
+++ 01. Import Users_Skeleton (Product Shop)/ProductShop/Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductShop.Data
+{
+    public static class Configuration
+    {
+        public const string ConnectionString =
+            @"Server=DESKTOP-L2KK2IT\SQLEXPRESS;Database=ProductShop;Integrated Security=True";
+    }
+}
