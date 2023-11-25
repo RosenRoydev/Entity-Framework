2458e91d1a23962b1af18bb874475a3485c7a955
RosenRoydev <122979363+RosenRoydev@users.noreply.github.com>
2023-11-25 23:34:12 +0200

    XML Processing CarDealer

---
+++ XML/01. Car Dealer/CarDealer/CarDealerProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+
+namespace CarDealer
+{
+    public class CarDealerProfile : Profile
+    {
+        public CarDealerProfile()
+        {
+            CreateMap<ImportSupplierDTO, Supplier>()
+             .ForMember(dest => dest.Name, opt => opt.MapFrom(src => src.Name))
+             .ForMember(dest => dest.IsImporter, opt => opt.MapFrom(src => src.IsImporter));
+        }
+    }
+}
---
+++ XML/01. Car Dealer/CarDealer/StartUp.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+            //9.
+            string suppliersXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            Console.WriteLine(ImportSuppliers(context,suppliersXml));
+        }
+        public static Mapper GetMapper()
+        {
+            var config = new MapperConfiguration(c => c.AddProfile<CarDealerProfile>());
+            return new Mapper(config);
+        }
+
+        //9.
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportSupplierDTO[]),new XmlRootAttribute("Suppliers"));
+            using var reader = new StringReader(inputXml);
+            ImportSupplierDTO[] importSuppliersDTO = (ImportSupplierDTO[])xmlSerializer.Deserialize(reader);
+            var mapper = GetMapper();
+            Supplier[] suppliers = mapper.Map<Supplier[]>(importSuppliersDTO);
+
+            context.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count()}";
+
+        }
+    }
+}
