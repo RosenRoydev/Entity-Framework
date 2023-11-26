8ed5480ac9360d646c997dca25aba48fc398763c
RosenRoydev <122979363+RosenRoydev@users.noreply.github.com>
2023-11-26 22:22:52 +0200

    XML processing

---
+++ XML/01. Car Dealer/CarDealer/DTOs/Import/ImportCarsDTO.cs
@@ -0,0 +1,26 @@
+﻿using CarDealer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Car")]
+    public class ImportCarsDTO
+    {
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("traveledDistance")]
+        public long TravaledDistance { get;set; }
+
+        [XmlArray("parts")]
+        public ImportPartIdDTO[] PartIds { get; set; }
+    }
+}
--- XML/01. Car Dealer/CarDealer/StartUp.cs
+++ XML/01. Car Dealer/CarDealer/StartUp.cs
@@ -11,11 +11,19 @@ namespace CarDealer
         public static void Main()
         {
             CarDealerContext context = new CarDealerContext();
-            context.Database.EnsureDeleted();
-            context.Database.EnsureCreated();
+            //context.Database.EnsureDeleted();
+            //context.Database.EnsureCreated();
             //9.
-            string suppliersXml = File.ReadAllText("../../../Datasets/suppliers.xml");
-            Console.WriteLine(ImportSuppliers(context,suppliersXml));
+            //string suppliersXml = File.ReadAllText("../../../Datasets/suppliers.xml");
+            //Console.WriteLine(ImportSuppliers(context,suppliersXml));
+
+            //10.
+            //  string partsXml = File.ReadAllText("../../../Datasets/parts.xml");
+            // Console.WriteLine(ImportParts(context,partsXml));
+
+            //11.
+            string carsXml = File.ReadAllText("../../../Datasets/cars.xml");
+            Console.WriteLine(ImportCars(context,carsXml));
         }
         public static Mapper GetMapper()
         {
@@ -37,6 +45,53 @@ namespace CarDealer
 
             return $"Successfully imported {suppliers.Count()}";
 
+        }
+
+        //10.
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportPartsDTO[]), new XmlRootAttribute("Parts"));
+            using var reader = new StringReader(inputXml);
+            ImportPartsDTO[] importParts = (ImportPartsDTO[])xmlSerializer.Deserialize(reader);
+            var suppliersIds= context.Suppliers.Select(x => x.Id).ToArray();
+
+            var mapper = GetMapper();
+            Part[]  parts = mapper.Map<Part[]>(importParts.Where(p =>suppliersIds.Contains(p.SupplierId)));
+            context.AddRange(parts);
+            context.SaveChanges();
+            return $"Successfully imported {parts.Count()}";
+        }
+
+        //11.
+        public static string ImportCars(CarDealerContext context, string inputXml)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(ImportCarsDTO[]), new XmlRootAttribute("Cars"));
+            using var reader = new StringReader(inputXml);
+            ImportCarsDTO[] importCarsDTOs = (ImportCarsDTO[])xmlSerializer.Deserialize(reader);
+
+            var mapper = GetMapper();
+            List<Car> cars = new List<Car>();
+
+            foreach (var carDTO in importCarsDTOs)
+            {
+               Car car = mapper.Map<Car>(carDTO);
+                var partIds = carDTO.PartIds.Select(c => c.Id).Distinct().ToArray();
+
+                List<PartCar> parts = new List<PartCar>();
+                foreach (var id in partIds)
+                {
+                    parts.Add(new PartCar
+                    {
+                        Car = car,
+                        PartId = id
+                    });
+                }
+                car.PartsCars = parts;
+                cars.Add(car);
+            }
+            context.AddRange(cars);
+            context.SaveChanges();
+            return $"Successfully imported {cars.Count}";
         }
     }
 }
